d15b8ff34a6d87e3f010186a060d8f6c4b6b89ff
Paul Ivan Dela Cruz <kmpkhai001@gmail.com>
2023-11-20 01:37:17 +0800

    first commit

--- src/features/auth/AuthSlice.js
+++ src/features/auth/AuthSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './counterAPI';
+import { fetchCount } from './AuthAPI';
 
 const initialState = {
   value: 0,
@@ -15,7 +15,7 @@ export const incrementAsync = createAsyncThunk(
   }
 );
 
-export const counterSlice = createSlice({
+export const AuthSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
@@ -40,8 +40,8 @@ export const counterSlice = createSlice({
   },
 });
 
-export const { increment } = counterSlice.actions;
+export const { increment } = AuthSlice.actions;
 
 export const selectCount = (state) => state.counter.value;
 
-export default counterSlice.reducer;
+export default AuthSlice.reducer;
